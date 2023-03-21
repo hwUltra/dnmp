ccad198e1c81824a8f34ec25a1a2269a629f1105
skywalkerwei <skywalkerwei@gmail.com>
2023-03-21 13:52:23 +0800

    update php80 swoole--openssl iconv

--- services/php8/extensions/install.sh
+++ services/php8/extensions/install.sh
@@ -60,13 +60,16 @@ isPhpVersionGreaterOrEqual()
 installExtensionFromTgz()
 {
     tgzName=$1
+    para1=
     extensionName="${tgzName%%-*}"
-
+    if [  $2 ]; then  
+        para1=$2
+    fi  
     mkdir ${extensionName}
     tar -xf ${tgzName}.tgz -C ${extensionName} --strip-components=1
-    ( cd ${extensionName} && phpize && ./configure && make ${MC} && make install )
+    ( cd ${extensionName} && phpize && ./configure ${para1} && make ${MC} && make install )
 
-    docker-php-ext-enable ${extensionName} $2
+    docker-php-ext-enable ${extensionName}
 }
 
 
@@ -199,7 +202,7 @@ if [[ -z "${EXTENSIONS##*,interbase,*}" ]]; then
 	#docker-php-ext-install ${MC} interbase
 fi
 
-if [[ -z "${EXTENSIONS##*,,*}" ]]; then
+if [[ -z "${EXTENSIONS##*,hprose,*}" ]]; then
     echo "---------- Install hprose ----------"
     printf "\n" | pecl install hprose
     docker-php-ext-enable hprose
@@ -513,15 +516,22 @@ fi
 
 if [[ -z "${EXTENSIONS##*,amqp,*}" ]]; then
     echo "---------- Install amqp ----------"
-    apk add --no-cache rabbitmq-c-dev
-    docker-php-ext-enable amqp
+    apk add --no-cache -U autoconf
+    apk add --no-cache -U gcc
+    apk add --no-cache -U linux-headers
+    apk add --no-cache -U libc-dev
+
+    apk add --no-cache --update --virtual .phpize-deps-configure $PHPIZE_DEPS \
+    && apk add rabbitmq-c-dev \
+    && printf '\n' | pecl install amqp \
+    && docker-php-ext-enable amqp \
+    && apk del .phpize-deps-configure
+    
 fi
 
 if [[ -z "${EXTENSIONS##*,redis,*}" ]]; then
     echo "---------- Install redis ----------"
-     installExtensionFromTgz redis-5.3.7
-    # pecl install redis
-    # docker-php-ext-enable redis
+    installExtensionFromTgz redis-5.3.7
 fi
 
 if [[ -z "${EXTENSIONS##*,apcu,*}" ]]; then
@@ -533,7 +543,7 @@ fi
 if [[ -z "${EXTENSIONS##*,memcached,*}" ]]; then
     echo "---------- Install memcached ----------"
     apk add --no-cache libmemcached-dev zlib-dev
-    pecl install memcached
+    pecl install memcached-3.2.3
     docker-php-ext-enable memcached
 fi
 
@@ -545,8 +555,7 @@ fi
 
 if [[ -z "${EXTENSIONS##*,xdebug,*}" ]]; then
     echo "---------- Install xdebug ----------"
-    pecl install xdebug
-    docker-php-ext-enable xdebug
+    installExtensionFromTgz xdebug-3.2.0
 fi
 
 if [[ -z "${EXTENSIONS##*,event,*}" ]]; then
@@ -577,9 +586,11 @@ fi
 
 
 if [[ -z "${EXTENSIONS##*,swoole,*}" ]]; then
-    echo "---------- Install swoole ----------"
-    pecl install swoole
-    docker-php-ext-enable swoole
+    echo "---------- Install swoole ----------"    
+    isPhpVersionGreaterOrEqual 8 0
+    if [[ "$?" = "1" ]]; then
+        installExtensionFromTgz swoole-5.0.2 --enable-openssl
+    fi
 fi
 
 if [[ -z "${EXTENSIONS##*,zip,*}" ]]; then

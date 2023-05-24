1fa9a6c36928f2e214bce49bead55a607136b991
skywalkerwei <skywalkerwei@gmail.com>
2023-05-24 15:51:41 +0800

    fix install

--- docker_install.sh
+++ docker_install.sh
@@ -112,7 +112,7 @@ do_install(){
 	sudo gpasswd -a ${USER} docker
 	mkdir -p $setup_path
 	cd $setup_path
-	git clone https://github.com/skywalkerwei/dnmp.git
+	git clone https://github.com/hwProMax/dnmp.git
   	cd dnmp
 	
 	# unzip ./services/php/extensions.zip
